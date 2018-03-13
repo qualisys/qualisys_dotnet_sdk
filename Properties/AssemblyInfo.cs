505ad57f6274039ea16a1adab476b99924109e4d
Morgan Larsson <morgan.larsson@qualisys.se>
2018-03-13 08:05:50 +0100

    New year

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Qualisys AB")]
 [assembly: AssemblyProduct("RTClientSDK")]
-[assembly: AssemblyCopyright("Copyright © Qualisys 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © Qualisys 2015-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.13.*")]
+[assembly: AssemblyVersion("1.15.*")]
