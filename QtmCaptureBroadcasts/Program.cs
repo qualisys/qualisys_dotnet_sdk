17483bc68ba711dd22d741e08ac5b93bed63f90f
Morgan Larsson <morgan.larsson@qualisys.se>
2020-10-27 13:56:09 +0100

    Added example on how capture udp broadcasts from qtm

---
+++ QtmCaptureBroadcasts/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Linq;
+
+namespace QtmCaptureBroadcasts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int qtmBroadcastPort = 8989;
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, qtmBroadcastPort);
+                socket.Bind(endPoint);
+
+                Console.WriteLine($"Listening for Qualisys Track Manager UDP broadcasts on port: {qtmBroadcastPort}");
+
+                byte[] buffer = new byte[1601];
+                while (true)
+                {
+                    var read = socket.Receive(buffer);
+                    if (read > 0)
+                    {
+                        var xml = Encoding.UTF8.GetString(buffer).Trim('\0');
+                        try
+                        {
+                            XDocument doc = XDocument.Parse(xml);
+                            Console.WriteLine(doc.ToString());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine(xml);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
