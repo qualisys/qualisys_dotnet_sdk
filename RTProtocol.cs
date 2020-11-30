4d6073efcded4a66fe7a45d180d602db2cc81b23
Emil Rosenquist <37864286+EmilRosenquist@users.noreply.github.com>
2020-11-30 11:27:42 +0100

    Received 0 means graceful closing.
    Co-authored-by: qjgn <45095781+qjgn@users.noreply.github.com>

--- RTProtocol.cs
+++ RTProtocol.cs
@@ -424,7 +424,7 @@ namespace QTMRealTimeSDK
                     {
                         // As long as we haven't received enough data, wait for more
                         received = mNetwork.Receive(ref data, receivedTotal, frameSize - receivedTotal, false, -1);
-                        if (received < 0)
+                        if (received <= 0)
                         {
                             if (!mNetwork.IsConnected())
                             {
